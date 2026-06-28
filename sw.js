4326c80454db73a8e39499c7fbd2d9266a77b2c2
oncloudmypage-01 <oncloudmypage@gmail.com>
2026-06-28 17:26:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -39,7 +39,7 @@ self.addEventListener('notificationclick', e => {
    è il modo in cui il Service Worker rileva l'aggiornamento e
    mostra il banner "Nuova versione disponibile" agli utenti
    che hanno già installato l'app. */
-const CACHE = 'italquake-v8';
+const CACHE = 'italquake-v8.1';
 const PRECACHE = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e =>
